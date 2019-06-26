3181da9fe6b66c762555a0b16d75043ba115d715
Mats Høvin <32478267+matshovin@users.noreply.github.com>
2019-06-26 23:20:38 +0200

    Update Afull.ino

--- Afull.ino
+++ Afull.ino
@@ -1,3 +1,5 @@
+// Arduino Duecemillanove eller hva den nå heter
+// Aktiv høy utgang her, motordriver har aktiv lav, men det blir vel det samme?
 #include <SPI.h>
 #include <SD.h>
 
@@ -6,48 +8,50 @@ boolean isRunning = false;
 
 void setup()
 {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  // Skrives ut alle på en gang for å spare tid
+  pinMode(2, OUTPUT); // X step/pulse
+  pinMode(3, OUTPUT); // X dir
+  pinMode(4, OUTPUT); // Y step/pulse
+  pinMode(5, OUTPUT); // Y dir
+  pinMode(6, OUTPUT); // Z step/pulse
+  pinMode(7, OUTPUT); // Z dir
 
+  // Brytere kjøres rett ut på pinne 2-7 når de aktiveres lav
   pinMode(A0, INPUT_PULLUP);
   pinMode(A1, INPUT_PULLUP);
   pinMode(A2, INPUT_PULLUP);
   pinMode(A3, INPUT_PULLUP);
   pinMode(A4, INPUT_PULLUP);
   pinMode(A5, INPUT_PULLUP);
+   
+  pinMode(9, INPUT_PULLUP); // Start jobb
+  pinMode(10, INPUT_PULLUP); // Stopp jobb
 
-  pinMode(9, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
-
-  Serial.begin(9600);
+  Serial.begin(9600); // info til PC
 }
 
 void loop()
 {
   // Leser brytere på A0-A5
-  byte analogIN = ~PINC; // active low to active high
+  byte analogIN = ~PINC; // omformer fra active low to active high
 
   // Kjører ut A0-A5 på XYZ dir og step pinner hvis trykket
   if ( analogIN )
   {
     // nuller også ut de pinner som ikke fins med shift
-
     byte stepDirUt = analogIN << 2;
 
     // Dir forst ut grunnet EasyDriver timing reqirements
     // passer pa aa ikke skrive over Rx Tx
 
+    // Dir ut 
     PORTD = (stepDirUt & 0b10101000) | (PORTD & 0b01010111);
     // Step ut
     PORTD = (stepDirUt & 0b01010100) | (PORTD & 0b10101011);
   }
 
   // Leser start / stop brytere
-  byte swStartStop = ~PINB;  // active low to active high
+  byte swStartStop = ~PINB;  // omformer fra active low to active high
   if ( swStartStop & 0b00000100 ) // Stop CNC run
     isRunning = false;
   else if ( swStartStop & 0b00000010 ) // start CNC run
@@ -58,7 +62,7 @@ void loop()
       delay(2000);
       myFile = SD.open("1.stp");
       if (myFile)
-        Serial.println("0.stp opened");
+        Serial.println("1.stp opened");
       isRunning = true;
     }
 
@@ -83,5 +87,5 @@ void loop()
   }
 
   delayMicroseconds(666);
-  PORTD = B10101011 & PORTD;  // nuller ut x,y,x step pins
+  PORTD = B10101011 & PORTD;  // nuller ut x,y,x step pins for å pulse
 }
